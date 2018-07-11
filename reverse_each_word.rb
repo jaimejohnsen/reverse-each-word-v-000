cfca27935693c14536dafa890b4cda87313e093a
Jaime Johnsen <drivetraindiva@gmail.com>
2018-07-11 00:15:32 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   reversed_sentence = []
-  sentence.split(' ').each do |reverse|
-    reversed_sentence << reverse
+  sentence.split(' ').each do |word|
+    reversed_sentence << word.reverse
   end
 end
