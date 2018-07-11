9d26a362bf5106cfccb002be7b2eeddda0d547e6
Jaime Johnsen <drivetraindiva@gmail.com>
2018-07-11 00:30:30 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
-#def reverse_each_word(sentence)
-#  reversed_sentence = []
-#  sentence.split(' ').each do |word|
-#    reversed_sentence << word.reverse
-#  end
-#  reversed_sentence.join(" ")
-#end
+def reverse_each_word(sentence)
+  reversed_sentence = []
+  sentence.split(' ').each do |word|
+    reversed_sentence << word.reverse
+  end
+  reversed_sentence.join(" ")
+end
 
 def reverse_each_word(sentence)
  sentence.split(" ").collect do |word|
